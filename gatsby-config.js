5cd8c0570df4ef9459947335ab395dac148ef31b
Sam McGovern <sam.mcgovern@salecycle.com>
2020-05-13 11:33:30 +0100

    install styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `styled-components`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
